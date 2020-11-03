f92374c5e0ea666f986b0217b42899dcc2a1f263
Roldan Barbante <rbarbante@mail.greenriverdev.com>
2020-11-03 02:14:18 -0500

    check login and create redirect

--- includes/check_login.php
+++ includes/check_login.php
@@ -4,3 +4,13 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 //session_start();
 
+session_start();
+//check user login
+
+if(!isset($_SESSION['un']))
+{
+    $_SESSION['page'] = $_SESSION['SCRIPT_URI'];
+
+    //Redirect user to login page
+    header("location: login.php");
+}
