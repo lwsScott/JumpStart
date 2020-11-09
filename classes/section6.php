868d621824fce59e39dbc34dfcbbe8ad2eed49cb
Roldan Barbante <rbarbante@mail.greenriverdev.com>
2020-11-08 20:58:12 -0500

    section 6 class update

---
+++ classes/section6.php
@@ -0,0 +1,593 @@
+<?php
+
+/*
+ * Analysis class for playbook website
+ * stores SWOT information
+ * 11/8/20
+ * @author Roldan Barbante
+ * @version 1.0
+ */
+
+
+class Sfction6
+{
+    //Declare instance variables
+    private $_answerID;
+    private $_f1;
+    private $_f2;
+    private $_f3;
+    private $_f4;
+    private $_f5;
+    private $_f6;
+    private $_f7;
+    private $_f8;
+    private $_f9;
+    private $_f10;
+    private $_f10a;
+    private $_f11;
+    private $_f12;
+    private $_f13;
+    private $_f14;
+    private $_f15;
+    private $_f16;
+    private $_f17;
+    private $_f18;
+    private $_f19;
+    private $_f20;
+    private $_f21;
+    private $_f22;
+    private $_f23;
+    private $_f24;
+    private $_f25;
+    private $_f26;
+    private $_f27;
+    private $_f28;
+    private $_f29;
+    private $_f30;
+    private $_f31;
+    private $_f32;
+    private $_f33;
+    private $_f34;
+    private $_f35;
+    private $_f36;
+    private $_f37;
+    private $_f38;
+    private $_f39;
+    private $_f40;
+    private $_f41;
+    private $_f42;
+    private $_f43;
+    private $_f44;
+    private $_f45;
+    private $_f46;
+
+
+
+
+
+
+    private $_userId;
+
+    // constructor
+
+    function __construct($f1, $f2, $f3, $f3a, $f4, $f5, $f6, $f7, $f8, $f9, $f10, $f10a,$f11,
+                         $f12, $f13, $f14, $f15, $f16, $f17, $f18, $f19, $f20, $f21, $f22,
+                         $f23, $f24, $f25, $f26, $f27, $f28, $f29, $f30, $f31, $f32, $f33, $f34,
+                         $f35, $f36, $f37, $f38, $f39, $f40, $f41, $f42, $f43, $f44, $f45, $f46 ,$userId)
+    {
+        $this->_f1 = $f1;
+        $this->_f2 = $f2;
+        $this->_f3 = $f3;
+        $this->_f4 = $f4;
+        $this->_f5 = $f5;
+        $this->_f6 = $f6;
+        $this->_f7 = $f7;
+        $this->_f8 = $f8;
+        $this->_f9 = $f9;
+        $this->_f10 = $f10;
+        $this->_f10a = $f10a;
+        $this->_f11 = $f11;
+        $this->_f12 = $f12;
+        $this->_f13 = $f13;
+        $this->_f14 = $f14;
+        $this->_f15 = $f15;
+        $this->_f16 = $f16;
+        $this->_f17 = $f17;
+        $this->_f18 = $f18;
+        $this->_f19 = $f19;
+        $this->_f20 = $f20;
+        $this->_f21 = $f21;
+        $this->_f22 = $f22;
+        $this->_f23 = $f23;
+        $this->_f24 = $f24;
+        $this->_f25 = $f25;
+        $this->_f26 = $f26;
+        $this->_f27 = $f27;
+        $this->_f28 = $f28;
+        $this->_f29 = $f29;
+        $this->_f30 = $f30;
+        $this->_f31 = $f31;
+        $this->_f32 = $f32;
+        $this->_f33 = $f33;
+        $this->_f34 = $f34;
+        $this->_f35 = $f35;
+        $this->_f36 = $f36;
+        $this->_f37 = $f37;
+        $this->_f38 = $f38;
+        $this->_f39 = $f39;
+        $this->_f40 = $f40;
+        $this->_f41 = $f41;
+        $this->_f42 = $f42;
+        $this->_f43 = $f43;
+        $this->_f44 = $f44;
+        $this->_f45 = $f45;
+        $this->_f46 = $f46;
+
+
+        $this->_userId = $userId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAnswfrID()
+    {
+        return $this->_answerID;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF1()
+    {
+        return $this->_f1;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF2()
+    {
+        return $this->_f2;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF3()
+    {
+        return $this->_f3;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF4()
+    {
+        return $this->_f4;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF5()
+    {
+        return $this->_f5;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF6()
+    {
+        return $this->_f6;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF7()
+    {
+        return $this->_f7;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF8()
+    {
+        return $this->_f8;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF9()
+    {
+        return $this->_f9;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF10()
+    {
+        return $this->_f10;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF10a()
+    {
+        return $this->_f10a;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF11()
+    {
+        return $this->_f11;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF12()
+    {
+        return $this->_f12;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF13()
+    {
+        return $this->_f13;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF14()
+    {
+        return $this->_f14;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF15()
+    {
+        return $this->_f15;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF16()
+    {
+        return $this->_f16;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF17()
+    {
+        return $this->_f17;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF18()
+    {
+        return $this->_f18;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF19()
+    {
+        return $this->_f19;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF20()
+    {
+        return $this->_f20;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF21()
+    {
+        return $this->_f21;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF22()
+    {
+        return $this->_f22;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF23()
+    {
+        return $this->_f23;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF24()
+    {
+        return $this->_f24;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF25()
+    {
+        return $this->_f25;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF26()
+    {
+        return $this->_f26;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF27()
+    {
+        return $this->_f27;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF28()
+    {
+        return $this->_f28;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF29()
+    {
+        return $this->_f29;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF30()
+    {
+        return $this->_f30;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF31()
+    {
+        return $this->_f31;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF32()
+    {
+        return $this->_f32;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF33()
+    {
+        return $this->_f33;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF34()
+    {
+        return $this->_f34;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF35()
+    {
+        return $this->_f35;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF36()
+    {
+        return $this->_f36;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF37()
+    {
+        return $this->_f37;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF38()
+    {
+        return $this->_f38;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF39()
+    {
+        return $this->_f39;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF40()
+    {
+        return $this->_f40;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF41()
+    {
+        return $this->_f41;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF42()
+    {
+        return $this->_f42;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF43()
+    {
+        return $this->_f43;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF44()
+    {
+        return $this->_f44;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF45()
+    {
+        return $this->_f45;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF46()
+    {
+        return $this->_f46;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF47()
+    {
+        return $this->_f47;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF48()
+    {
+        return $this->_f48;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF49()
+    {
+        return $this->_f49;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF50()
+    {
+        return $this->_f50;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF51()
+    {
+        return $this->_f51;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF52a()
+    {
+        return $this->_f52a;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF52b()
+    {
+        return $this->_f52b;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF52c()
+    {
+        return $this->_f52c;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getF52d()
+    {
+        return $this->_f52d;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserId()
+    {
+        return $this->_userId;
+    }
+}
