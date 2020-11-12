50e51e060daa3c38cf1d3c2810f4bdeff591385c
Lewis Scott <lscott19@mail.greenriver.edu>
2020-11-11 21:28:03 -0500

    refactoring code for reproducability

---
+++ model/data-layer.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * data layer for playbook website
+ * stores arrays of data
+ * 11/11/20
+ * filename https://lscott.greenriverdev.com/355/JumpStart/model/data-layer.php
+ * @author Lewis Scott
+ * @version 1.0
+ */
+
+// get valid indoor interests
+function getSection1()
+{
+    // create an array called section1
+    // set the hive variables
+    $section1 = array('a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'a9', 'a10', 'a11',
+        'a12', 'a13', 'a14', 'a15', 'a16', 'a17', 'a18');
+    return $section1;
+}
